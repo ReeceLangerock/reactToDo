2fc19c46dd75ecee3df6ecd2e6e503088d7521b2
ReeceLangerock <ReeceLangerock@gmail.com>
2017-06-28 21:03:23 -0500

    add showCompleted and searchText reducers

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,44 @@
+import expect from "expect";
+var actions = require("actions");
+
+
+describe("Actions", () => {
+  it("should generate searchText action", () => {
+    var action = {
+      type: "SET_SEARCH_TEXT",
+      searchText: "Some search text"
+    };
+
+    var response = actions.setSearchText(action.searchText);
+    expect(response).toEqual(action);
+  });
+
+  it("should generate add todo action", () => {
+    var action = {
+      type: "ADD_TODO",
+      text: "Thing to do"
+    };
+
+    var response = actions.addToDo(action.text);
+    expect(response).toEqual(action);
+  });
+
+  it("should generate toggleToDo action", () => {
+    var action = {
+      type: "TOGGLE_TODO",
+      id: 1
+    };
+
+    var response = actions.toggleToDo(action.id);
+    expect(response).toEqual(action);
+  });
+
+  it("should generate toggleShowCompleted action", () => {
+    var action = {
+      type: "TOGGLE_SHOW_COMPLETED"
+    };
+
+    var response = actions.toggleShowCompleted(action);
+    expect(response).toEqual(action);
+  });
+});
